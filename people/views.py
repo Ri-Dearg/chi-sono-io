a585a70d36d9bca8114f5273a328395c448d658f
Ri-Dearg <sheridan.rp@gmail.com>
2020-07-14 00:38:01 +0000

    Home page view created

--- people/views.py
+++ people/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+# from django.contrib.postgres.search import (SearchQuery,
+#                                             SearchRank,
+#                                             SearchVector)
 
-# Create your views here.
+from .models import Person
+
+
+class PersonListView(ListView):
+    """Renders the home page with a Products List.
+    If there is a GET request, performs a search."""
+    model = Person
+    context_object_name = 'people'
