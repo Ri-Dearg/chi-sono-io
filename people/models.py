bea6b935ad3cbf3156e5664697edcd25f20eba96
Ri-Dearg <sheridan.rp@gmail.com>
2020-09-26 00:09:46 +0000

    Updated Person Model
    Removed pattern fields, added thumb field, images are auto resized.

--- people/models.py
+++ people/models.py
@@ -1,6 +1,11 @@
+import sys
 from django.db import models
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.utils import timezone
 from django.contrib.postgres import fields
+
+from PIL import Image
+from io import BytesIO
 
 
 class Person(models.Model):
@@ -10,17 +15,45 @@ class Person(models.Model):
     story = models.TextField()
     video = models.URLField()
     portrait = models.ImageField(upload_to='portraits')
+    thumb = models.ImageField(upload_to='portraits/thumbs')
     pattern1 = models.ImageField(upload_to='patterns', blank=True, default='')
     pattern1_bg = models.CharField(max_length=7, blank=True, default='')
     pattern1_txt = models.CharField(max_length=7, blank=True, default='')
-    pattern2 = models.ImageField(upload_to='patterns', blank=True, default='')
-    pattern2_bg = models.CharField(max_length=7, blank=True, default='')
-    pattern2_txt = models.CharField(max_length=7, blank=True, default='')
-    pattern3 = models.ImageField(upload_to='patterns', blank=True, default='')
-    pattern3_bg = models.CharField(max_length=7, blank=True, default='')
-    pattern3_txt = models.CharField(max_length=7, blank=True, default='')
     tags = fields.ArrayField(models.CharField(max_length=40), size=10)
     date = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        """ Image resizing, snippet repurposed from:
+        https://djangosnippets.org/snippets/10597/ """
+
+        img = Image.open(self.portrait)
+        img_format = img.format.lower()
+
+        # Prevents images from being copied on every save
+        # will save a new copy on an upload
+        person = Person.objects.get(pk=self.id)
+        if (self.portrait.name != person.portrait.name) \
+                or (not person):
+            # Image is resized
+            output_size = (106, 139)
+            img = img.resize(size=(output_size))
+
+            # Converts format while in memory
+            output = BytesIO()
+            img.save(output, format=img_format)
+            output.seek(0)
+
+            # Replaces the Imagefield value with the newly converted image
+            self.thumb = InMemoryUploadedFile(
+                output,
+                'ImageField',
+                f'{self.portrait.name.split(".")[0]}.{img_format}',
+                'image/jpeg', sys.getsizeof(output),
+                None)
+
+            super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.name}'
