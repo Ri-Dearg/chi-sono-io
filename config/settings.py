1cc22658fd6b7e6a53106b8a796e02f8ebc7e598
Ri-Dearg <sheridan.rp@gmail.com>
2020-09-27 16:35:18 +0000

    Corrected typo in custom_storage

--- config/settings.py
+++ config/settings.py
@@ -197,10 +197,10 @@ if not DEVELOPMENT:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
     STATICFILES_LOCATION = 'static'
-    STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'config.custom_storage.StaticStorage'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
 
     # Media Settings
     MEDIAFILES_LOCATION = 'media'
-    DEFAULT_FILE_STORAGE = 'config.custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'config.custom_storage.MediaStorage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
