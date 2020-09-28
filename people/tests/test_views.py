05b0ac5a8e94d8d59e629fe1f6dc3545c1f1abd5
Ri-Dearg <sheridan.rp@gmail.com>
2020-09-28 11:36:33 +0000

    Added tests for new search fucntions

--- people/tests/test_views.py
+++ people/tests/test_views.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.postgres.search import (SearchQuery,
+                                            SearchRank,
+                                            SearchVector)
 
 from people.models import Person
 
@@ -22,6 +25,7 @@ class TestPeopleViews(TestCase):
         return super().setUp()
 
     def test_render_index(self):
+        response = self.client.get('/?page=2')
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'people/person_list.html')
@@ -32,7 +36,42 @@ class TestPeopleViews(TestCase):
         self.assertQuerysetEqual(response.context['people'],
                                  Person.objects.all(),
                                  transform=lambda x: x)
-    
+
+    def test_search_functions(self):
+        response = self.client.get('/?query=')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'people/person_list.html')
+        self.assertTemplateUsed(response, 'people/person_detail.html')
+        self.assertTemplateUsed(response, 'people/includes/person_box.html')
+        self.assertTemplateUsed(response, 'people/includes/detail_modal.html')
+
+        response = self.client.get('/?query=tag')
+        self.user_query = 'tag'
+        self.assertEqual(response.status_code, 200)
+        self.vector = SearchVector(
+                    'name',
+                    'blurb',
+                    'story',
+                    weight='B') + SearchVector(
+                        'tags', weight='A')
+        self.query = SearchQuery(self.user_query)
+        self.rank = SearchRank(self.vector, self.query)
+
+        # Returns search results in order of rank,
+        # removing items with no rank.
+        self.assertQuerysetEqual(response.context['people'],
+                                 Person.objects.annotate(
+                    rank=self.rank).order_by(
+                    '-rank').filter(rank__gt=0),
+                                 transform=lambda x: x)
+
+        response = self.client.get('/?query=tag?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'people/person_list.html')
+        self.assertTemplateUsed(response, 'people/person_detail.html')
+        self.assertTemplateUsed(response, 'people/includes/person_box.html')
+        self.assertTemplateUsed(response, 'people/includes/detail_modal.html')
+
     def test_ajax_render_detail(self):
         person = Person.objects.latest('date')
         response = self.client.get('/')
